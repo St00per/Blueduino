50da57cfd7748cac6fe1b1d76665535374c839f8
Кирилл Штеффен <kirillshteffen@gmail.com>
2018-12-24 12:18:52 +0600

    page control both directions scrolling and some view details corrections

--- Blueduino/UserDevicesViewController.swift
+++ Blueduino/UserDevicesViewController.swift
@@ -48,8 +48,46 @@ extension UserDevicesViewController: UICollectionViewDataSource, UICollectionVie
         return cell
     }
     
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        pageControl.currentPage += 1
-    }
+//    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+//        pageControl.currentPage += 1
+//    }
     
-}
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        targetContentOffset.pointee = scrollView.contentOffset
+        var indexes = self.collectionView.indexPathsForVisibleItems
+        indexes.sort()
+        let index = indexes.first
+        guard var localIndex = index else { return }
+        let cell = self.collectionView.cellForItem(at: localIndex)
+        let position = self.collectionView.contentOffset.x - (cell?.frame.origin.x ?? 0)
+        let comparedCellWidth = (cell?.frame.size.width ?? 0)/2
+        
+        //fast swipe
+        if velocity.x > 1
+             {
+                pageControl.currentPage += 1
+            localIndex.row = (localIndex.row) + 1
+            self.collectionView.scrollToItem(at: localIndex , at: .left, animated: true )
+            
+        }
+        
+        if velocity.x < -1 {
+            pageControl.currentPage -= 1
+            self.collectionView.scrollToItem(at: localIndex, at: .left, animated: true)
+            
+        }
+        
+        //slow swipe
+        if velocity.x > -1 , velocity.x < 1,
+            position > comparedCellWidth {
+            localIndex.row = (localIndex.row) + 1
+            self.collectionView.scrollToItem(at: localIndex , at: .left, animated: true )
+            
+        }
+        
+        if velocity.x > -1 , velocity.x < 1,
+            position < comparedCellWidth {
+            self.collectionView.scrollToItem(at: localIndex , at: .left, animated: true )
+            
+        }
+    }}
